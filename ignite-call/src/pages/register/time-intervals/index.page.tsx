58eb1795007e9264b93a55af76c4315cb6953f34
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-03-20 11:40:41 -0300

    Time-interval page created

---
+++ ignite-call/src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,66 @@
+import { TextInput } from "@ignite-ui/react";
+import {
+  Button,
+  Checkbox,
+  Heading,
+  Multistep,
+  Text,
+} from "@itoddy-ui/react/dist";
+
+import { ArrowRight } from "phosphor-react";
+import { Container, Header } from "../styles";
+
+import {
+  IntervalBox,
+  IntervalContainer,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+} from "./styles";
+
+export default function TimeIntervals() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Quase lá</Heading>
+        <Text>
+          Defina o intervalo de horário que você está disponível em cada dia da
+          semana.
+        </Text>
+
+        <Multistep size={4} currentStep={3} />
+      </Header>
+
+      <IntervalBox as="form">
+        <IntervalContainer>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Segunda-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Terça-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+        </IntervalContainer>
+
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </IntervalBox>
+    </Container>
+  );
+}
