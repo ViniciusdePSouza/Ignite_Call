e6ac10bbc7e07f2093e3cca413f2a9288b5b4466
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-03-24 14:00:21 -0300

    Form: useFOrm config done

--- ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -2,9 +2,24 @@ import { CalendarBlank, Clock } from "phosphor-react";
 import { ConfirmForm, FormActions, FormHeader } from "./styles";
 import { Button, Text, TextArea } from "@itoddy-ui/react/dist";
 import { TextInput } from "@ignite-ui/react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const ConfirmStepFormSchema = z.object({
+    name: z.string().min(3),
+    email: z.string().min(7),
+    observations: z.string()
+})
+
+type ConfirmStepFormData = z.infer<typeof ConfirmStepFormSchema>
 
 export default function ConfirmStep() {
     function handleConfirmScheduling() {}
+
+    const { register, handleSubmit, formState: {errors, isSubmitting} } = useForm<ConfirmStepFormData>({
+        resolvers: zodResolver(ConfirmStepFormSchema)
+    })
 
     return (
         <ConfirmForm as='form' onSubmit={handleConfirmScheduling}>
@@ -20,22 +35,22 @@ export default function ConfirmStep() {
                 </Text>
 
                 <label>
-                    <Text size='sm'>Nome Copleto</Text>
-                    <TextInput placeholder='Seu Nome'/>
+                    <Text size='sm'>Nome Completo</Text>
+                    <TextInput placeholder='Seu Nome' {...register('name')}/>
                 </label>
                 <label>
                     <Text size='sm'>Endereço de e-mail</Text>
-                    <TextInput type='email' placeholder='Seu Nome'/>
+                    <TextInput type='email' placeholder='Seu Nome'  {...register('email')}/>
                 </label>
                 <label>
                     <Text size='sm'>Observações</Text>
-                    <TextArea  />
+                    <TextArea  {...register('observations')}/>
                 </label>
             </FormHeader>
 
             <FormActions>
                 <Button type='button' variant='tertiary'>Cancelar</Button>
-                <Button type='submit'>Confirmar</Button>
+                <Button type='submit' disabled={isSubmitting}>Confirmar</Button>
             </FormActions>
         </ConfirmForm>
     )
