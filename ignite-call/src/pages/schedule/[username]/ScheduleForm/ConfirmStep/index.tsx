064e0c795466ec56cf1e1b0446c66e540a85d40d
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-03-28 11:18:10 -0300

    Component lib added

--- ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -6,52 +6,65 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-const ConfirmStepFormSchema = z.object({
-    name: z.string().min(3),
-    email: z.string().min(7),
-    observations: z.string()
-})
+const confirmStepFormSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  observations: z.string().nullable(),
+});
 
-type ConfirmStepFormData = z.infer<typeof ConfirmStepFormSchema>
+type ConfirmStepFormData = z.infer<typeof confirmStepFormSchema>;
 
 export default function ConfirmStep() {
-    function handleConfirmScheduling() {}
+  function handleConfirmScheduling() {
+    console.log('ola')
+  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ConfirmStepFormData>({
+    resolver: zodResolver(confirmStepFormSchema)
+  });
 
-    const { register, handleSubmit, formState: {errors, isSubmitting} } = useForm<ConfirmStepFormData>({
-        resolvers: zodResolver(ConfirmStepFormSchema)
-    })
+  return (
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2023
+        </Text>
 
-    return (
-        <ConfirmForm as='form' onSubmit={handleConfirmScheduling}>
-            <FormHeader>
-                <Text>
-                    <CalendarBlank/>
-                    22 de Setembro de 2023
-                </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
 
-                <Text>
-                    <Clock/>
-                    18:00h
-                </Text>
+        <label>
+          <Text size="sm">Nome Completo</Text>
+          <TextInput placeholder="Seu Nome" {...register("name")} />
+        </label>
+        <label>
+          <Text size="sm">Endereço de e-mail</Text>
+          <TextInput
+            type="email"
+            placeholder="Seu Nome"
+            {...register("email")}
+          />
+        </label>
+        <label>
+          <Text size="sm">Observações</Text>
+          <TextArea {...register("observations")} />
+        </label>
+      </FormHeader>
 
-                <label>
-                    <Text size='sm'>Nome Completo</Text>
-                    <TextInput placeholder='Seu Nome' {...register('name')}/>
-                </label>
-                <label>
-                    <Text size='sm'>Endereço de e-mail</Text>
-                    <TextInput type='email' placeholder='Seu Nome'  {...register('email')}/>
-                </label>
-                <label>
-                    <Text size='sm'>Observações</Text>
-                    <TextArea  {...register('observations')}/>
-                </label>
-            </FormHeader>
-
-            <FormActions>
-                <Button type='button' variant='tertiary'>Cancelar</Button>
-                <Button type='submit' disabled={isSubmitting}>Confirmar</Button>
-            </FormActions>
-        </ConfirmForm>
-    )
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit" disabled={isSubmitting}>
+          Confirmar
+        </Button>
+      </FormActions>
+    </ConfirmForm>
+  );
 }
